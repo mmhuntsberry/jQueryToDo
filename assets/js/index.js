b201450d2dcf11cc43e78705fa322a4166bd139e
Matt <mmhuntsberry@gmail.com>
2018-10-29 10:39:31 -0500

    Added functionality to toggle done class when clicking on an li and the fading out and removal of the an li when clicking on its child span.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
+// Adds the done class clicked li
 $('li').on('click', function() {
   $(this).toggleClass('done');
+});
+
+// fades out and removes the parent li of the
+// clicked on span
+$('span').on('click', function(e) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
 });
