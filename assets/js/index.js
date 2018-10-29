24bb87ed05f53111e4f7b92126de2ba3d91e01e3
Matt <mmhuntsberry@gmail.com>
2018-10-29 11:17:06 -0500

    Add the create To-Do functionaility.  With the event listeners responding to new To Dos.  This works be using the dynamicChild parameter in the on even handler listening on the parent element.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,27 @@
 // Adds the done class clicked li
-$('li').on('click', function() {
+// listens for a li click inside a ul.  This is for dynamicChildren.
+$('ul').on('click', 'li', function() {
   $(this).toggleClass('done');
 });
 
 // fades out and removes the parent li of the
 // clicked on span
-$('span').on('click', function(e) {
+// listens for a span click in a ul.  This is for dynamicChildren
+$('ul').on('click', 'span', function(e) {
   $(this)
     .parent()
     .fadeOut(500, function() {
       $(this).remove();
     });
   event.stopPropagation();
+});
+
+// listens for the enter keypress
+// then appends a new li to the ul
+$('input[type="text"]').on('keypress', function(e) {
+  if (event.which === 13) {
+    console.log($(this).val());
+    $('ul').append('<li><span>X</span> ' + $(this).val() + '</li>');
+    $(this).val('');
+  }
 });
